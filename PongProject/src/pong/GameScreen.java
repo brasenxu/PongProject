258ca9d6eb251beb52ff9867629e1ea70a20263e
dlatka <dlatka@CARY.home>
2021-06-17 10:51:41 -0400

    new

--- PongProject/src/pong/GameScreen.java
+++ PongProject/src/pong/GameScreen.java
@@ -5,20 +5,100 @@ import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
 
-public class GameScreen extends JFrame {
+public class GameScreen extends JFrame implements KeyListener {
 	
-	Game screen;
+	 	private static int WINDOWWIDTH = 1000;
+	    private static int WINDOWHEIGHT = 1000;
+	    private static int RECTWIDTH1 = 100;
+	    private static int RECTWIDTH2 = 100;
+	    private static int RECTHEIGHT1 = 300;
+	    private static int RECTHEIGHT2 = 300;
+	    private double xValue1;
+	    private double yValue1;
+	    private double yVelocity1;
+	    private double xValue2;
+	    private double yValue2;
+	    private double yVelocity2;
 	
-	GameScreen(){
+	
+	public GameScreen(){
 		
-		screen = new Game();
-		
-		this.setResizable(false);
-		this.setBackground(Color.BLACK);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.pack();
-		this.setVisible(true);
-		this.setLocationRelativeTo(null);
+		super("Pong");
+		xValue1 = 0;
+		yValue1 = 0;
+		xValue2 = WINDOWWIDTH-RECTWIDTH2;
+		yValue2 = 0;
+		yVelocity1 = 0;
+		yVelocity2 = 0;
+		addKeyListener(this);
 	}
 	
-}
+	public void keyTyped(KeyEvent e) {
+	
+		if(e.getKeyChar() == 'i') {
+            yVelocity2 = -0.5;
+        }
+        else if(e.getKeyChar() == 'k') {
+            yVelocity2 = 0.5;
+        }
+        else if(e.getKeyChar() == 'w') {
+            yVelocity1 = -0.5;
+        }
+        else if(e.getKeyChar() == 's') {
+            yVelocity1 = 0.5;
+        }
+	}
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyChar() == 'i' || e.getKeyChar() == 'k'){
+			yVelocity2 = 0;	
+		}
+		if(e.getKeyChar() == 'w' || e.getKeyChar() == 's'){
+			yVelocity1 = 0;	
+		}	
+	}
+	
+	public void keyPressed(KeyEvent e) {
+	}
+	
+	public void paint(Graphics g) {
+		g.clearRect(0, 0, WINDOWWIDTH, WINDOWHEIGHT);
+		g.fillRect((int)xValue1, (int)yValue1, RECTWIDTH1, RECTHEIGHT1);
+		g.fillRect((int)xValue2, (int)yValue2, RECTWIDTH2, RECTHEIGHT2);
+		
+		movement();
+		repaint();
+	}
+	public void movement() {
+		if (yValue1 < 0) {
+			yValue1 = 1;
+		}
+		else if (yValue2 < 0) {
+			yValue2 = 1;
+		}
+		else if (yValue1 > WINDOWHEIGHT-RECTHEIGHT1) {
+			yValue1 = WINDOWHEIGHT-RECTHEIGHT1;
+		}
+		else if (yValue2 > WINDOWHEIGHT-RECTHEIGHT2) {
+			yValue2 = WINDOWHEIGHT-RECTHEIGHT2;
+		}
+		else {
+			yValue1 += yVelocity1;
+			yValue2 += yVelocity2;
+		}
+			
+	}
+		
+	public static void main(String [] args) {
+		GameScreen screen = new GameScreen();
+		screen.setSize(WINDOWWIDTH, WINDOWHEIGHT);
+        screen.setLocationRelativeTo(null);
+        screen.setResizable(false); 
+        screen.setVisible(true); 
+        screen.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		
+	}
+	
+		
+	}
+	
+
