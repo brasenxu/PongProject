30556d25ae724bbd3ad9a9eb918f1792a2f60ec8
MoroccanGemsbok <71042550+MoroccanGemsbok@users.noreply.github.com>
2021-06-21 10:47:14 -0400

    Menu comments

--- PongProject/src/Menu.java
+++ PongProject/src/Menu.java
@@ -1,43 +1,49 @@
-
+//import needed package
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 
-
+//Menu class
 public class Menu {
-	boolean isAi;
 	
-	//create rectangles as buttons
-	public Rectangle aiButton = new Rectangle(PongPanel.WIDTH/2-50, 150, 100, 50);
-	public Rectangle playButton = new Rectangle(PongPanel.WIDTH/2-50, 250, 100, 50);
-	public Rectangle helpButton = new Rectangle(PongPanel.WIDTH/2-96, 350, 200, 50);
-	public Rectangle exitButton = new Rectangle(PongPanel.WIDTH-100, PongPanel.HEIGHT-75, 75, 50);
+	//create rectangles for the menu buttons
+	public Rectangle aiButton = new Rectangle(PongPanel.WIDTH/2-50, 150, 100, 50); //singleplayer button
+	public Rectangle playButton = new Rectangle(PongPanel.WIDTH/2-50, 250, 100, 50); //multiplayer button
+	public Rectangle helpButton = new Rectangle(PongPanel.WIDTH/2-96, 350, 200, 50); //instructions button
+	public Rectangle exitButton = new Rectangle(PongPanel.WIDTH-100, PongPanel.HEIGHT-75, 75, 50); //exit button
 	
-	
+	/* draws the menu on the screen
+	 * pre: none
+	 * post: the main menu have been printed onto the screen
+	 */
 	public void draw(Graphics g) {
-		Graphics2D g2d = (Graphics2D) g;
-		//create 2 fonts
+		Graphics2D g2d = (Graphics2D) g; //create a 2d graphics class
+		
+		//create 2 fonts for the menu
 		Font font1 = new Font("arial", Font.BOLD, 50);
 		Font font2 = new Font("arial", Font.PLAIN, 30);
-		g.setFont(font1);
+		
+		g.setFont(font1); //set the first font
 		g.setColor(Color.white); //set font colour to white
 
 		g.drawString("A Game of Pong", 300, 100); //title string
 		
-		g.setFont(font2);
+		g.setFont(font2); //set the second font	
 		
 		//draw rectangles
-		g.setColor(Color.red);
-		g2d.fill(exitButton);
-		g.setColor(Color.white);
+		g.setColor(Color.red); //set colour to red
+		g2d.fill(exitButton); //draw red rectangle for exit button
+		g.setColor(Color.white); //set colour to white
+		
+		//draw buttons
 		g2d.draw(exitButton);
 		g2d.draw(aiButton);
 		g2d.draw(playButton);
 		g2d.draw(helpButton);
 		
-		//label of buttons
+		//button labels
 		g.drawString("1 vs AI", aiButton.x + 5, aiButton.y + 37);
 		g.drawString("1 vs 1", playButton.x + 10, playButton.y + 37);
 		g.drawString("Intructions", helpButton.x + 29, helpButton.y + 37);
