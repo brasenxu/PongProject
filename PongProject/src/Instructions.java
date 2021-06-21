db5da049b340f72c66b6d3d6687484324265aebf
MoroccanGemsbok <71042550+MoroccanGemsbok@users.noreply.github.com>
2021-06-21 09:53:02 -0400

    formatted instructions

--- PongProject/src/Instructions.java
+++ PongProject/src/Instructions.java
@@ -10,7 +10,7 @@ public class Instructions {
 		
 		//create 2 fonts
 		Font font1 = new Font("arial", Font.BOLD, 50);
-		Font font2 = new Font("arial", Font.PLAIN, 30);
+		Font font2 = new Font("arial", Font.PLAIN, 24);
 		Font font3 = new Font("arial", Font.PLAIN, 15);
 		g.setFont(font1);
 		
@@ -20,10 +20,11 @@ public class Instructions {
 		g.setFont(font2);
 		
 		//label of buttons
-		g.drawString("Move Cyan player with 'w' and 's' keys",250 , 200 );
-		g.drawString("Move Yellow player with 'up' and 'down' arrows",200 ,275 );
-		g.drawString("First player to score 10 goals WINS!",273 ,350 );
-		g.drawString("Made by: Brasen & Nathan", 325, 475);
+		g.drawString("Move Cyan player paddle with 'w' and 's' keys",250 , 200 );
+		g.drawString("If playing 1v1, move Yellow player with 'up' and 'down' arrows",170 ,250 );
+		g.drawString("Hit the ball with the paddles to score in opponents ends", 210, 300);
+		g.drawString("First player to score 10 goals WINS!",295 ,350 );
+		g.drawString("Made by: Brasen & Nathan", 350, 475);
 		g2d.draw(BackButton);
 		
 		g.setFont(font3);
