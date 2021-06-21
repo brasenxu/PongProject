0f932edc55288e7bf998793c3e6558d8477761f3
MoroccanGemsbok <71042550+MoroccanGemsbok@users.noreply.github.com>
2021-06-21 10:16:32 -0400

    added comments to AIpaddle class

--- PongProject/src/AIpaddle.java
+++ PongProject/src/AIpaddle.java
@@ -1,29 +1,40 @@
-
+//import needed package
 import java.awt.*;
-import java.awt.event.*;
 
+//AI paddle class 
 public class AIpaddle extends Rectangle{
 	
-	int yVelocity;
-	int speed = 5; //set speed
-	Ball ball;
+	//define variables needed in class
+	int yVelocity; //direction of the paddle (y direction)
+	Ball ball; //set ball class variable
 	
+	/* constructor
+	 * pre: none
+	 * post: An AIpaddle object has been created with values x, y, PADDLE_WIDTH, PADDLE_HEIGHT, and a Ball variable ball
+	 */
 	AIpaddle(int x, int y, int PADDLE_WIDTH, int PADDLE_HEIGHT, Ball b){
 		//create rectangle object
 		super(x,y,PADDLE_WIDTH,PADDLE_HEIGHT);
-		ball = b;
-
+		ball = b; //set ball to b
 	}
 	
-	//updates position of rectangle
+	/* updates the position of rectangle
+	 * pre: none
+	 * post: the location of the rectangle has been changed
+	 */
 	public void move() {		
 		yVelocity = (int)((ball.yVelocity)/1.25);
 		y= y + yVelocity;
 	}
 	
+	/* draws the rectangle paddle on the screen
+	 * pre: none
+	 * post: the filled rectangle has been drawn, and has a colour of orange (looks yellow in the game screen)
+	 */
 	public void draw(Graphics g) {
 		//set colour of rectangles
 		g.setColor(Color.orange);
+		//draw rectangle
 		g.fillRect(x, y, width, height);
 	}
 }
